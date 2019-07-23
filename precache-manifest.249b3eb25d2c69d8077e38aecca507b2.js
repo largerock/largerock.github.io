5ec628311f2139b6b0fda4c325c849765bdfc20c
George Price <largerock1@gmail.com>
2019-07-24 00:05:51 +1200

    React App starter

---
+++ precache-manifest.249b3eb25d2c69d8077e38aecca507b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3e7dc2501b311c856d6eb1b8743f16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cca946b8dba30b3ef2a",
+    "url": "/static/js/2.b3c82f8b.chunk.js"
+  },
+  {
+    "revision": "26175824fc19669fa421",
+    "url": "/static/js/main.5b002ca3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
