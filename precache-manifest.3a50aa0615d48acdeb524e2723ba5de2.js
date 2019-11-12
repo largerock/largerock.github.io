d8a59c958c8d3c992ca552fe581a5c0fff916ab5
largerock <largerock1@gmail.com>
2019-11-12 22:00:19 +1300

    New prod release of largerock.com

---
+++ precache-manifest.3a50aa0615d48acdeb524e2723ba5de2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab89eaee65daafb31eb53602f7ead3b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83a16c43a71a7afc2dfd",
+    "url": "/static/css/main.33850586.chunk.css"
+  },
+  {
+    "revision": "0e481b1fc4f015ef56bb",
+    "url": "/static/js/2.56f5f51b.chunk.js"
+  },
+  {
+    "revision": "83a16c43a71a7afc2dfd",
+    "url": "/static/js/main.4134989d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
